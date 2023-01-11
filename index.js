5af12a14b467949967f8cba61112702f28b78869
DannieMo <danielowiri4zoe@gmail.com>
2023-01-11 09:34:17 +0100

    Clean up code

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const maincontainer = document.querySelector(".awesomebooks");
+const maincontainer = document.querySelector('.awesomebooks');
 const addmarkup = () => {
-  maincontainer.innerHTML = "";
-  const data = JSON.parse(localStorage.getItem("todo")) || [];
-  const ul = document.createElement("ul");
+  maincontainer.innerHTML = '';
+  const data = JSON.parse(localStorage.getItem('todo')) || [];
+  const ul = document.createElement('ul');
   data.forEach((todo) => {
-    const li = document.createElement("li");
+    const li = document.createElement('li');
     li.innerHTML = `<span>${todo.todo}</span><button class='removetodo' data-id='${todo.id}'>Remove todo</button>`;
     ul.append(li);
   });
 
   maincontainer.appendChild(ul);
-  document.querySelectorAll(".removetodo").forEach((Element) => {
-    Element.addEventListener("click", (e) => {
+  document.querySelectorAll('.removetodo').forEach((Element) => {
+    Element.addEventListener('click', (e) => {
       e.preventDefault();
-      const id = Element.getAttribute("data-id");
-      let Storagedata = JSON.parse(localStorage.getItem("todo")) || [];
+      const id = Element.getAttribute('data-id');
+      let Storagedata = JSON.parse(localStorage.getItem('todo')) || [];
       Storagedata = Storagedata.filter((todo) => todo.id !== parseInt(id));
-      localStorage.setItem("todo", JSON.stringify(Storagedata));
+      localStorage.setItem('todo', JSON.stringify(Storagedata));
       addmarkup();
     });
   });
 };
 addmarkup();
 
-document.querySelector(".todosbmit").addEventListener("submit", (e) => {
+document.querySelector('.todosbmit').addEventListener('submit', (e) => {
   e.preventDefault();
-  const Storagedata = JSON.parse(localStorage.getItem("todo")) || [];
+  const Storagedata = JSON.parse(localStorage.getItem('todo')) || [];
   const data = {
-    todo: document.querySelector(".todoinput").value,
+    todo: document.querySelector('.todoinput').value,
     id: Storagedata.length + 1,
   };
   const newdata = [...Storagedata, data];
-  localStorage.setItem("todo", JSON.stringify(newdata));
+  localStorage.setItem('todo', JSON.stringify(newdata));
   addmarkup();
 });

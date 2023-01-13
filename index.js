905d707130c9bbd60a65fe41e4b0a216b0849319
ActiveApps <activeapps@MACs-MacBook-Air.local>
2023-01-13 10:47:48 +0300

    add navigation

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 /* eslint-disable */
+class Books{
+  constructor(id,book,author){
+    this.id=id
+    this.todo=book
+    this.author=author
+  }
+}
+const removebook=()=>{
+  document.querySelectorAll('.removetodo').forEach((Element) => {
+    Element.addEventListener('click', (e) => {
+      e.preventDefault();
+      const id = Element.getAttribute('data-id');
+      let Storagedata = JSON.parse(localStorage.getItem('todo')) || [];
+      Storagedata = Storagedata.filter((todo) => todo.id !== parseInt(id));
+      localStorage.setItem('todo', JSON.stringify(Storagedata));
+      addmarkup();
+    });
+  });
+}
+
 const maincontainer = document.querySelector('.awesomebooks');
+
 const addmarkup = () => {
   maincontainer.innerHTML = '';
   const data = JSON.parse(localStorage.getItem('todo')) || [];
@@ -11,27 +32,52 @@ const addmarkup = () => {
   });
 
   maincontainer.appendChild(ul);
-  document.querySelectorAll('.removetodo').forEach((Element) => {
-    Element.addEventListener('click', (e) => {
-      e.preventDefault();
-      const id = Element.getAttribute('data-id');
-      let Storagedata = JSON.parse(localStorage.getItem('todo')) || [];
-      Storagedata = Storagedata.filter((todo) => todo.id !== parseInt(id));
-      localStorage.setItem('todo', JSON.stringify(Storagedata));
-      addmarkup();
-    });
-  });
+  removebook()
 };
 addmarkup();
 
-document.querySelector('.todosbmit').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const Storagedata = JSON.parse(localStorage.getItem('todo')) || [];
-  const data = {
-    todo: document.querySelector('.todoinput').value,
-    id: Storagedata.length + 1,
-  };
-  const newdata = [...Storagedata, data];
-  localStorage.setItem('todo', JSON.stringify(newdata));
-  addmarkup();
-});
+const addbook=()=>{
+  document.querySelector('.todosbmit').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const Storagedata = JSON.parse(localStorage.getItem('todo')) || [];
+    let book=new Books(Storagedata.length + 1,document.querySelector('.todoinput').value,document.querySelector('.author').value)
+    console.log(book)
+    const newdata = [...Storagedata, book];
+    localStorage.setItem('todo', JSON.stringify(newdata));
+    addmarkup();
+  });  
+}
+addbook()
+
+
+const add=document.querySelector(".add")
+const list=document.querySelector(".list")
+const contact=document.querySelector(".contact")
+//This is where the different container selections start from
+const formcontainer=document.querySelector(".formcontainer")
+const bookscontainer=document.querySelector(".bookscontainer")
+const contactcontainer=document.querySelector(".contactcontainer")
+
+formcontainer.style.display="flex"
+formcontainer.style.flexDirection="column"
+bookscontainer.style.display="none"
+contactcontainer.style.display="none"
+
+add.addEventListener("click",()=>{
+  formcontainer.style.display="flex"
+  bookscontainer.style.display="none"
+  contactcontainer.style.display="none"
+})
+
+list.addEventListener("click",()=>{
+  formcontainer.style.display="none"
+  bookscontainer.style.display="flex"
+  contactcontainer.style.display="none"
+})
+
+contact.addEventListener("click",()=>{
+  formcontainer.style.display="none"
+  bookscontainer.style.display="none"
+  contactcontainer.style.display="flex"
+})
+
